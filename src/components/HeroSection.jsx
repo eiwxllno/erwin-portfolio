d88fb1467728f74ffd52a7bf5bdf2ae6af8a8a93
eiwxllno <erwinllano95@gmail.com>
2025-03-19 13:49:06 +0800

    update hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,14 @@
 function HeroSection() {
   return (
     <div id="hero" className="flex items-center justify-center flex-col py-20">
+      {/* Add a div for the circular image */}
+      <div className="w-24 h-24 md:w-32 md:h-32 rounded-full overflow-hidden mb-4">
+        <img
+          src="https://i.imgur.com/4Om1C3l.jpeg" //
+          alt="Erwin"
+          className="w-full h-full object-cover"
+        />
+      </div>
       <div className="text-center">
         <h1 className="text-2xl md:text-4xl mb-1 md:mb-3 text-customColor dark:text-customColor font-semibold">
           Hi, This is Erwin 👋
