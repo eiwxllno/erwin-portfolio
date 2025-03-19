e1da15db82fee0b554e179cf60b3bcc2b0356e75
eiwxllno <erwinllano95@gmail.com>
2025-03-19 14:00:10 +0800

    workitem add animation

--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -1,35 +1,35 @@
-import React from 'react';
+import React from "react";
 
 function WorkItem({ imgUrl, title, tech, workUrl }) {
-	return (
-		<a
-			href={workUrl}
-			target="_blank"
-			rel="noreferrer"
-			className="bg-slate-300 dark:bg-slate-800 rounded-lg overflow-hidden"
-		>
-			<img
-				src={imgUrl}
-				alt="work"
-				className="w-full h-36 md:h-48 object-cover"
-			/>
-			<div className="w-full p-5 text-gray-600 dark:text-gray-300 ">
-				<h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">
-					{title}
-				</h3>
-				<p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm ">
-					{tech.map(item => (
-						<span
-							key={item}
-							className="inline-block px-2 py-1 bg-slate-200 dark:bg-slate-900 rounded-md"
-						>
-							{item}
-						</span>
-					))}
-				</p>
-			</div>
-		</a>
-	);
+  return (
+    <a
+      href={workUrl}
+      target="_blank"
+      rel="noreferrer"
+      className="bg-slate-300 dark:bg-slate-800 rounded-lg overflow-hidden transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg hover:shadow-slate-400/50 dark:hover:shadow-slate-900/50"
+    >
+      <img
+        src={imgUrl}
+        alt="work"
+        className="w-full h-36 md:h-48 object-cover"
+      />
+      <div className="w-full p-5 text-gray-600 dark:text-gray-300">
+        <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">
+          {title}
+        </h3>
+        <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
+          {tech.map((item) => (
+            <span
+              key={item}
+              className="inline-block px-2 py-1 bg-slate-200 dark:bg-slate-900 rounded-md"
+            >
+              {item}
+            </span>
+          ))}
+        </p>
+      </div>
+    </a>
+  );
 }
 
 export default WorkItem;
