a864cdd2ba3c0ebfdce37e68b9b991eda987b936
eiwxllno <erwinllano95@gmail.com>
2025-03-19 11:49:19 +0800

    update images

--- src/data/works.js
+++ src/data/works.js
@@ -1,51 +1,50 @@
-import InvestCalcImg from "../images/InvestCalc.png";
-import TictactoeImg from "../images/Tictactoe.JPG.jpg";
-import BankistImg from "../images/Bankist.jpg";
-import GuesNumImg from "../images/GuessNumber.jpg";
-import ImpactGapImg from "../images/Impactgapyear.jpg";
-import SahogImg from "../images/Sahog.jpg";
-import OmnifoodImg from "../images/Omnifood.jpg";
-
 export default [
   {
     title: "Investment Calculator",
-    imgUrl: InvestCalcImg,
+    imgUrl:
+      "https://drive.google.com/file/d/1klA0QI74bGidituXqhaHqQmZqf5-xzKL/view?usp=sharing",
     tech: ["ReactJS", "HTML5", "CSS3", "JS", "Vite"],
     workUrl: "https://investment-calculator-erwinllano.vercel.app/",
   },
   {
     title: "Tic-Tac-Toe",
-    imgUrl: TictactoeImg,
+    imgUrl:
+      "https://drive.google.com/file/d/1_o6FheFQ5STOKaa2Br47HaRl5cwF9NAh/view?usp=sharing",
     tech: ["ReactJS", "HTML5", "CSS3", "JS", "Vite"],
     workUrl: "https://tic-tac-toe-erwinllano.vercel.app/",
   },
   {
     title: "Sahog",
-    imgUrl: SahogImg,
+    imgUrl:
+      "https://drive.google.com/file/d/1zidaysn3pTs7srnVsR20z8B27HuXAoMW/view?usp=sharing",
     tech: ["HTML5", "CSS3", "JavaScript", "MVC", "API"],
     workUrl: "https://sahog.netlify.app/",
   },
   {
     title: "Guess Number",
-    imgUrl: GuesNumImg,
+    imgUrl:
+      "https://drive.google.com/file/d/1oO2YzB7vceAeVW-E1WRbcXsNbDzYOg85/view?usp=sharing",
     tech: ["HTML5", "CSS3", "JavaScript"],
     workUrl: "https://guess-number-erwin.netlify.app/",
   },
   {
     title: "Impact Gap Year",
-    imgUrl: ImpactGapImg,
+    imgUrl:
+      "https://drive.google.com/file/d/1ehHZjLrCstQqzolBx4ZHvg2H6IE_dvUI/view?usp=sharing",
     tech: ["HTML5", "CSS3", "Wordpress", "PHP"],
     workUrl: "https://impactgapyear.com/",
   },
   {
     title: "Bank Landing Website",
-    imgUrl: BankistImg,
+    imgUrl:
+      "https://drive.google.com/file/d/1L4ODYUTecOh8PDtqpKJJNs3cJlGT7bET/view?usp=sharing",
     tech: ["HTML5", "CSS3", "JavaScript"],
     workUrl: "https://bankist-erwin.netlify.app/",
   },
   {
     title: "Omnifood",
-    imgUrl: OmnifoodImg,
+    imgUrl:
+      "https://drive.google.com/file/d/11hVyph1YkUnJOyRwRoFTXz50qKyJvD7v/view?usp=sharing",
     tech: ["HTML5", "CSS3", "JavaScript"],
     workUrl: "https://omnifood-erwin.netlify.app/",
   },
