b07a9bbe9285b734ac2d4d161e3e5bae1bc58eb6
eiwxllno <erwinllano95@gmail.com>
2025-03-19 12:01:50 +0800

    update images

--- src/data/works.js
+++ src/data/works.js
@@ -1,50 +1,43 @@
 export default [
   {
     title: "Investment Calculator",
-    imgUrl:
-      "https://drive.google.com/file/d/1klA0QI74bGidituXqhaHqQmZqf5-xzKL/view?usp=sharing",
+    imgUrl: "https://imgur.com/5jpeOqJ",
     tech: ["ReactJS", "HTML5", "CSS3", "JS", "Vite"],
     workUrl: "https://investment-calculator-erwinllano.vercel.app/",
   },
   {
     title: "Tic-Tac-Toe",
-    imgUrl:
-      "https://drive.google.com/file/d/1_o6FheFQ5STOKaa2Br47HaRl5cwF9NAh/view?usp=sharing",
+    imgUrl: "https://imgur.com/AmNEab8",
     tech: ["ReactJS", "HTML5", "CSS3", "JS", "Vite"],
     workUrl: "https://tic-tac-toe-erwinllano.vercel.app/",
   },
   {
     title: "Sahog",
-    imgUrl:
-      "https://drive.google.com/file/d/1zidaysn3pTs7srnVsR20z8B27HuXAoMW/view?usp=sharing",
+    imgUrl: "https://imgur.com/bmKQfxu",
     tech: ["HTML5", "CSS3", "JavaScript", "MVC", "API"],
     workUrl: "https://sahog.netlify.app/",
   },
   {
     title: "Guess Number",
-    imgUrl:
-      "https://drive.google.com/file/d/1oO2YzB7vceAeVW-E1WRbcXsNbDzYOg85/view?usp=sharing",
+    imgUrl: "https://imgur.com/KGN8fdu",
     tech: ["HTML5", "CSS3", "JavaScript"],
     workUrl: "https://guess-number-erwin.netlify.app/",
   },
   {
     title: "Impact Gap Year",
-    imgUrl:
-      "https://drive.google.com/file/d/1ehHZjLrCstQqzolBx4ZHvg2H6IE_dvUI/view?usp=sharing",
+    imgUrl: "https://imgur.com/kVnn8TC",
     tech: ["HTML5", "CSS3", "Wordpress", "PHP"],
     workUrl: "https://impactgapyear.com/",
   },
   {
     title: "Bank Landing Website",
-    imgUrl:
-      "https://drive.google.com/file/d/1L4ODYUTecOh8PDtqpKJJNs3cJlGT7bET/view?usp=sharing",
+    imgUrl: "https://imgur.com/hUsOvsv",
     tech: ["HTML5", "CSS3", "JavaScript"],
     workUrl: "https://bankist-erwin.netlify.app/",
   },
   {
     title: "Omnifood",
-    imgUrl:
-      "https://drive.google.com/file/d/11hVyph1YkUnJOyRwRoFTXz50qKyJvD7v/view?usp=sharing",
+    imgUrl: "https://imgur.com/8MSfPtK",
     tech: ["HTML5", "CSS3", "JavaScript"],
     workUrl: "https://omnifood-erwin.netlify.app/",
   },
